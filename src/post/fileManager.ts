523c76bf651b4b63960e989f00db1391d0cae61a
Saul Burgos <saulburgos6@gmail.com>
2023-07-12 11:36:25 -0400

    add loggin

--- src/post/fileManager.ts
+++ src/post/fileManager.ts
@@ -1,19 +1,25 @@
 import * as fs from 'node:fs/promises';
 import { Post } from './post.model';
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class FileManager {
+  private readonly logger = new Logger(FileManager.name);
+
   public async readFileRp(): Promise<Post[]> {
+    this.logger.log('Reading file...');
     const response = await fs.readFile('./src/db.json', 'utf8');
+    this.logger.log('parsing file to JSON..');
     const post: Post[] = JSON.parse(response);
+    this.logger.log('Returning Posts...');
     return post;
   }
 
   public async WriteFileRp(post: Post): Promise<void> {
     const postList = await this.readFileRp();
+    this.logger.log('adding post at list of Post ');
     postList.push(post);
-
+    this.logger.log('saving post...');
     fs.writeFile('./src/db.json', JSON.stringify(postList));
   }
 }
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,17 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { Post } from './post.model';
 import { FileManager } from './fileManager';
 import { randomUUID } from 'crypto';
 
 @Injectable()
 export class PostService {
+  private readonly logger = new Logger(PostService.name);
   constructor(private fileManager: FileManager) {}
 
   public async findAll(): Promise<Post[]> {
+    this.logger.log('Finding Post');
     return await this.fileManager.readFileRp();
   }
   public create(post: Post): void {
     post.id = randomUUID();
+    this.logger.log('Save Post');
     this.fileManager.WriteFileRp(post);
   }
 }
