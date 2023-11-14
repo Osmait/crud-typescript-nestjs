5b052957eaedad55a6c01bd91aa67f515467a5d1
osmait <saulburgos6@gmail.com>
2023-11-13 20:10:05 -0400

    add prometheus

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,9 @@ import { AppService } from './app.service';
 import { PostModule } from './post/post.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-
+import { PrometheusModule } from '@willsoto/nestjs-prometheus';
 @Module({
-  imports: [PostModule, AuthModule, UsersModule],
+  imports: [PostModule, AuthModule, UsersModule, PrometheusModule.register()],
   controllers: [AppController],
   providers: [AppService],
 })
